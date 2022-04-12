fd13f7baf6e88dac5c2abbdca704c7c29bcc85d3
FataLity2328 <maksim-kargin@bk.ru>
2022-04-12 18:04:23 +0300

    Created view part and finalized adapter

--- app/src/main/java/com/github/qsubq/a22bytetestapplication/screen/news/NewsAdapter.kt
+++ app/src/main/java/com/github/qsubq/a22bytetestapplication/screen/news/NewsAdapter.kt
@@ -5,25 +5,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.github.qsubq.a22bytetestapplication.R
+import com.github.qsubq.a22bytetestapplication.databinding.ItemNewsLayoutBinding
 import com.github.qsubq.a22bytetestapplication.model.Article
+import com.squareup.picasso.Picasso
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
-    class NewsViewHolder(view : View) : RecyclerView.ViewHolder(view)
+    class NewsViewHolder(val binding: ItemNewsLayoutBinding) : RecyclerView.ViewHolder(binding.root)
 
-    var NewsList = emptyList<Article>()
+    private var newsList = emptyList<Article>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news_layout, parent, false)
-        return NewsViewHolder(view)
+        val itemBinding = ItemNewsLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsViewHolder(itemBinding)
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        (holder.binding as ItemNewsLayoutBinding).apply {
+            tvTitleItem.text = newsList[position].title
+            tvDescriptionItem.text = newsList[position].description
+            Picasso.get().load(newsList[position].urlToImage).into(imgItem)
+        }
 
     }
 
     override fun getItemCount(): Int {
-        return NewsList.size
+        return newsList.size
     }
 
-
+    fun setNewsList(list : List<Article>){
+        newsList = list
+        notifyDataSetChanged()
+    }
 }
